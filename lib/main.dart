c46012cfcda9ab5cd5b1c6584689d68871b3bf28
TK_Tossakan <148406638+TkTossakan@users.noreply.github.com>
2025-07-03 20:42:30 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Flutter App'),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 74, 75, 72),
+          foregroundColor: Colors.white,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/2.jpg',
+                errorBuilder: (context, error, stackTrace) {
+                  return const Text('โหลดรูปไม่สำเร็จ');
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Student Kritsana Sukchaiyat',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'นักศึกษาสาขาวิทยาการคอมพิวเตอร์ คณะศิลปศาสตร์และวิทยาศาสตร์\nมหาวิทยาลัยราชภัฏศรีสะเกษ',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+
+              // ⭐⭐⭐⭐⭐ + 170 Reviews ในแถวเดียว
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.star, color: Colors.orange),
+                  Icon(Icons.star, color: Colors.orange),
+                  Icon(Icons.star, color: Colors.orange),
+                  Icon(Icons.star, color: Colors.orange),
+                  Icon(Icons.star, color: Colors.orange),
+                  SizedBox(width: 10),
+                  Text(
+                    "170 Reviews",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              // แถวข้อมูล Phone, Email, Social
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: const [
+                        Icon(Icons.phone, color: Color.fromARGB(255, 76, 175, 80)),
+                        SizedBox(height: 5),
+                        Text('PHONE'),
+                        Text('096-195-2755'),
+                      ],
+                    ),
+                    Column(
+                      children: const [
+                        Icon(Icons.email, color: Color.fromARGB(255, 33, 150, 243)),
+                        SizedBox(height: 5),
+                        Text('EMAIL'),
+                        Text('tktossakan@gmail.com'),
+                      ],
+                    ),
+                    Column(
+                      children: const [
+                        Icon(Icons.share, color: Color.fromARGB(255, 156, 39, 176)),
+                        SizedBox(height: 5),
+                        Text('SOCIAL'),
+                        Text('@Tossakan'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
